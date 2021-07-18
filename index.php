3486535566c2218e787e00df4e43781a0e67e9af
Godspower Jackson <69468196+GodspowerJacks@users.noreply.github.com>
2021-07-18 15:27:48 +0100

    Added Bootstrap to the code

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
     <title>SearchMe</title>
 </head>
 <script>
@@ -26,10 +27,14 @@
 </script>
 <body>
 
-<form action="" method="get">
-    <input type="text" name="" size="30" style="border:2px solid grey;" onkeyup="showResult(this.value)" id="">
-    <input type="submit" value="Search">
-    <div id="livesearch"></div>
+<form action="" method="get" class="pt-4 mt-5 p-4">
+    <h1 class="text-center pt-4 text-bold">SEARCH ME</h1>
+    <div class="form-group col-md-6 col-12 m-auto p-2">
+        <input type="text" name="" size="30"placeholder="Search Me......" class="form-control col-md-6 col-12" onkeyup="showResult(this.value)" id="">
+        <input type="submit" class="btn btn-success col-md-12 pt-2 mt-2 col-12 m-auto" value="Search">
+    </div>
+    
+    <div id="livesearch" class="m-auto col-md-6"></div>
 </form>
     
 </body>
